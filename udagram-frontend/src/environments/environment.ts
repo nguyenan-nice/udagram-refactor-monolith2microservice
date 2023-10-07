bae04007501b95d66f8f9f510972966fde8a470f
nguyenan-nice <nguyenan.it88@gmail.com>
2023-10-07 17:23:00 +0700

    updated

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'My Udagram Microservice Application',
-  apiHost: 'http://localhost:8080/api/v0/'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
